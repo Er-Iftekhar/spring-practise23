c038b78026e64ec32fc92abe08ff1902bb741fb2
SyedIftekharuddin <syedy636@gmail.com>
2020-06-30 13:25:21 +0200

    BootStrapData class for temporary static data.

---
+++ src/main/java/com/example/springpractise/bootstrap/BootstrapDaa.java
@@ -0,0 +1,12 @@
+package com.example.springpractise.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootstrapDaa implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
